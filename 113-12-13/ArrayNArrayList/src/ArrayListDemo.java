c02dc0f307ed1a697473bdf34f7d60ce4df2c615
A112223006 <a112223006@mail.shu.edu.tw>
2024-12-13 12:04:54 +0800

    113-12-13

---
+++ 113-12-13/ArrayNArrayList/src/ArrayListDemo.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public  static void main(String[] args){
+        ArrayList<String> List = new ArrayList<>();
+
+        list.add("Apple");
+    }
+
+}
